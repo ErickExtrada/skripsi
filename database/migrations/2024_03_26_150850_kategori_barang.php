48582a5724ee7ccbb4fc6899a2c8364afc39683d
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2024-03-30 18:30:22 +0700

    fixing: issue for typo schema kategori_barang

--- database/migrations/2024_03_26_150850_kategori_barang.php
+++ database/migrations/2024_03_26_150850_kategori_barang.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('kategori_baramg', function (Blueprint $table) {
+        Schema::create('kategori_barang', function (Blueprint $table) {
             $table->id();
             $table->string('kategori_barang');
             $table->string('kode_barang');
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('kategori_baramg');
+        Schema::dropIfExists('kategori_barang');
     }
 };
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(2)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test2@example.com',
+            'password' => '123456',
+            'role' => 'pengelola_gudang',
+        ]);
     }
 }
